20fc8b7ef24d2a5f4b2b7a9ec06707f8baf58776
Brian Lin <brianhlin@gmail.com>
2019-01-04 14:28:27 -0600

    Test RPM builds

---
+++ test/test_inside_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -xe
+
+BUILD_ENV=$2
+
+ls -l /home
+
+# Clean the yum cache
+yum -y clean all
+yum -y clean expire-cache
+yum -y update  # Update the OS packages
+
+# First, install all the needed packages.
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VERSION}.noarch.rpm
+
+# Broken mirror?
+echo "exclude=mirror.beyondhosting.net" >> /etc/yum/pluginconf.d/fastestmirror.conf
+
+yum -y install yum-plugin-priorities rpm-build python-devel gcc-c++
+
+./build/build_all
